04a4152a2a2246189e6e9d2a4ac5a83d6f8e25f1
erikborges013 <erikborgesofficial@gmail.com>
2025-05-31 23:24:32 -0300

    update modulo js 31/05

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+//Aqui abaico a gente captura o valor digitado no campo de digitação
 const inputItem = document.getElementById("input-item");
+//Aqui embaixo a gente captura o evento do botão clicado
+const adicionarItem = document.getElementById("adicionar-item");
 
+console.log(inputItem.value);
 
-console.log(`A linguagem escolhida é: ${linguagemescolhida}`);
+//Aqui embaixo a gente executa uma função através de um evento que nesse caso está sendo evento de clique
+//adicionarItem.addEventListener("click", (evento) => {
+    //Aqui embaixo esse código impede que a página recarregue quando o botão for clicado
+    //evento.preventDefault();
+    //E  aqui embaixo a gente exibe o valor digitado no input através do .value
+    //console.log(inputItem.value);
+//});
