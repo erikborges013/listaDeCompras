17c56b41d23cc6b51c7845e09f6008b1ddf33945
Erik Borges <erikborgesofficial@gmail.com>
2025-06-02 03:40:18 -0300

    Adicionei uma funcionalidade de adicionar os elementos e os itens a tela

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-//Aqui abaico a gente captura o valor digitado no campo de digitação
+const lista = document.getElementById("lista-de-compras");
+
 const inputItem = document.getElementById("input-item");
-//Aqui embaixo a gente captura o evento do botão clicado
+
 const adicionarItem = document.getElementById("adicionar-item");
 
-console.log(inputItem.value);
+let contador = 0;
 
-//Aqui embaixo a gente executa uma função através de um evento que nesse caso está sendo evento de clique
-//adicionarItem.addEventListener("click", (evento) => {
-    //Aqui embaixo esse código impede que a página recarregue quando o botão for clicado
-    //evento.preventDefault();
-    //E  aqui embaixo a gente exibe o valor digitado no input através do .value
-    //console.log(inputItem.value);
-//});
+adicionarItem.addEventListener("click", (evento) => {
+    evento.preventDefault();
+
+    if (inputItem.value === "") {
+        alert("Campo vazio! Por favor digite algum item para inserir.");
+        return;
+    }
+        
+
+    const itemDaLista = document.createElement("li");
+    const containerItemDaLista = document.createElement("div");
+    containerItemDaLista.classList.add("lista-item-container");
+    const inputCheckBox = document.createElement("input");
+    inputCheckBox.type = "checkbox";
+    inputCheckBox.id = "checkbox-" + contador++;
+    const nomeItem = document.createElement("p");
+    nomeItem.innerHTML = inputItem.value;
+
+    containerItemDaLista.appendChild(inputCheckBox);
+    containerItemDaLista.appendChild(nomeItem);
+    itemDaLista.appendChild(containerItemDaLista);
+
+    lista.appendChild(itemDaLista);
+
+});
